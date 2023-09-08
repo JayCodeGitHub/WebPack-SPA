7b29b4b2c1ed7748aae00f39db3a649d480ce011
JayCode Developer <JayCode.contakt@gmail.com>
2023-09-08 18:36:42 +0200

    Add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: "development",
     entry: "./src/app.ts",
     output: {
         filename: 'app.js',
@@ -9,6 +11,11 @@ module.exports = {
     resolve: {
         extensions: ['.ts', '.js'],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './public/index.html', 
+        }),
+      ],
     module: {
         rules: [
             {
